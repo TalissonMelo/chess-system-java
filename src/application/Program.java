3a753296a1522b6ab910929e07185ae42c223e46
TalissonMelo <talissonmelorodrigues@gmail.com>
2019-11-09 18:03:27 -0300

    Implementando classe UI e printBoard

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,13 @@
 package application;
 
-import boardGame.Board;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Board tabuleiro = new Board(8,8);
+		
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 	}
 
 }
