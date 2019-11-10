b88906d035f9a9dd084c9276611d76c72d4bbe12
TalissonMelo <talissonmelorodrigues@gmail.com>
2019-11-10 09:24:52 -0300

    Programação defensiva BoardException

--- src/boardGame/Board.java
+++ src/boardGame/Board.java
@@ -5,12 +5,15 @@ public class Board {
 	private int rows;
 	private int columns;
 	private Piece[][] pieces;
-	
+
 	public Board() {
-		
+
 	}
 
 	public Board(int rows, int columns) {
+		if (rows < 1 || columns < 1) {
+			throw new BoardException("Erro ao criar tabuleiro: conter ao menos 1 linha e 1 coluna");
+		}
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
@@ -20,30 +23,44 @@ public class Board {
 		return rows;
 	}
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
 	public int getColumns() {
 		return columns;
 	}
 
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
-	
 	public Piece piece(int row, int column) {
+		if (!positionExists(row, column)) {
+			throw new BoardException("Posição inexistente no tabuleiro");
+		}
 		return pieces[row][column];
 	}
-	
+
 	public Piece piece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Posição inexistente no tabuleiro");
+		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
-	
-	public void placePiece(Piece piece , Position position) {
+
+	public void placePiece(Piece piece, Position position) {
+		if (thereIsAPiece(position)) {
+			throw new BoardException("já existe uma peça na posição " + position);
+		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
 	}
-	
-	
+
+	private boolean positionExists(int row, int column) {
+		return row >= 0 && row < rows && column >= 0 && column < columns;
+	}
+
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+	}
+
+	public boolean thereIsAPiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Posição inexistente no tabuleiro");
+		}
+		return piece(position) != null;
+	}
 }
