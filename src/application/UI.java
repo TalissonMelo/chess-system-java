f467b492092b5a5b531166bb038653f52f7b90b5
TalissonMelo <talissonmelorodrigues@gmail.com>
2019-11-10 11:11:40 -0300

    Tratamento de exceções e limpando a tela

--- src/application/UI.java
+++ src/application/UI.java
@@ -30,6 +30,12 @@ public class UI {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+	// https://stackoverflow.com/questions/2979383/java-clear-the-console
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
+
 	public static ChessPosition readChessPosition(Scanner sc) {
 		try {
 			String s = sc.nextLine();
@@ -38,7 +44,7 @@ public class UI {
 
 			return new ChessPosition(column, row);
 		} catch (RuntimeException e) {
-			throw new InputMismatchException("Erro lendo posicão: Valores validos são de a1 a h8");
+			throw new InputMismatchException("Erro lendo posicão: Valores validos são de a1 a h8.");
 		}
 	}
 
